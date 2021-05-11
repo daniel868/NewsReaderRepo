c4a5f43ca9548733b27e1bb9964496496ffcde7f
daniel868 <sincalexandrudaniel@gmail.com>
2021-05-11 21:32:03 +0300

    First Commit

---
+++ app/src/main/java/com/SincAlexandruDaniel/newsreader/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.SincAlexandruDaniel.newsreader.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
